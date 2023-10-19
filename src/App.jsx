984c3bea8886e40ec9780d1d2c668efb16b1fc83
Sagar Kokitkar <101313853+kokitkar1@users.noreply.github.com>
2023-10-19 07:51:35 +0530

    Create file Folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import SignIn from './pages/SignIn.jsx'
+import SignUp from './pages/SignUp.jsx'
+import About from './pages/About.jsx'
+import Profile from './pages/Profile.jsx'
 
-export default function App() {
+
+
+const App = () => {
   return (
-    <h1 className='text-red-500'>App</h1>
+    <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/profile' element={<Profile />} />
+        </Routes>
+    </BrowserRouter>
   )
 }
+
+export default App
