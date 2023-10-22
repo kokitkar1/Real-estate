d221c50c0d8f60315517bf065aa08a54529dafcc
Sagar Kokitkar <101313853+kokitkar1@users.noreply.github.com>
2023-10-22 14:07:41 +0530

    Connected To DB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,24 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+// mongoose.connect(process.env.MONGO)
+mongoose.connect("mongodb+srv://skokitkar500:OK2-CsKvsMI@real-estate.3g5npmp.mongodb.net/real-estate?retryWrites=true&w=majority").then(()=>{
+    console.log("Connected to MongoDB");
+}).catch((err)=>{
+    console.log(err);
+})
+
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!!`)
+    }
+)
