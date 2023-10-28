77c74eeac872a40b04a137c4646068854248f4f6
Sagar Kokitkar <101313853+kokitkar1@users.noreply.github.com>
2023-10-28 22:33:03 +0530

    Create Listing API

--- api/index.js
+++ api/index.js
@@ -1,41 +1,27 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import cookieParser from 'cookie-parser'
-import userRouter from './routes/user.route.js'
-import authRouter from './routes/auth.route.js'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 
-dotenv.config()
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL).then(() => {console.log("Connected to MongoDB");}).catch((err) => {console.log(err);});
 
-mongoose.connect(process.env.MONGO_URL).then(()=>{
-    console.log("Connected to MongoDB");
-}).catch((err)=>{
-    console.log(err);
-})
-
-
-const app = express()
+const app = express();
 app.use(express.json());
-app.use(cookieParser())
-const port = 3000
- 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.use("/api/user", userRouter)
-app.use("/api/auth", authRouter)
+app.use(cookieParser());
 
+app.get("/", (req, res) => res.send("Hello World!"));
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message,
-    })
-})
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({ success: false, statusCode, message });
+});
 
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}!!`)
-    }
-)
+app.listen(process.env.PORT, () => {console.log(`Example app listening on port ${process.env.PORT}!!`);});
