646486efde98a76781c8d90dd27929a1dc921c36
Sagar Kokitkar <101313853+kokitkar1@users.noreply.github.com>
2023-11-02 07:30:02 +0530

    Create Update listing API

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,5 +1,6 @@
 import Listing from "../models/listing.model.js"
 import { errorHandler } from "../utils/error.js";
+import mongoose from "mongoose";
 
 export const createListing = async (req, res, next) => {
     try {
@@ -25,6 +26,9 @@ export const deleteListing = async (req,res,next) => {
 
 
 export const updateListing = async (req,res,next) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        return next(errorHandler(400, 'Invalid ObjectId'));
+    }
     const listing = await Listing.findById(req.params.id);
     if(!listing){ return next(errorHandler(404, 'Listing not found!')) }
     if(req.user.id !== listing.userRef){ return next(errorHandler(401, 'Your are Not authorized!')) }
@@ -40,3 +44,17 @@ export const updateListing = async (req,res,next) => {
     }
 }
 
+export const getListing = async (req,res,next) => {
+    try {
+        if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+            return next(errorHandler(400, 'Invalid ObjectId'));
+        }
+        const listing = await Listing.findById(req.params.id);
+        if(!listing){
+            return next(errorHandler(404, 'Listing not found!'))
+        }
+        res.status(200).json(listing)
+    } catch (error) {
+        next(error)
+    }
+}
