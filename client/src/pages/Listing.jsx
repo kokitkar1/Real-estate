4756374066af5c62b7df834b557e3ac2602b6301
Sagar Kokitkar <101313853+kokitkar1@users.noreply.github.com>
2023-11-04 07:03:30 +0530

    Add contact landlord functionality

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useSelector } from 'react-redux';
 import { Swiper, SwiperSlide } from 'swiper/react'
 import SwiperCore from 'swiper';
 import { Navigation } from 'swiper/modules';
 import 'swiper/css/bundle'
-import {FaBath, FaBed, FaChair, FaMapMarkedAlt, FaMapMarkerAlt, FaParking, FaShare,} from 'react-icons/fa';
-// import Contact from '../components/Contact';
+import {FaBath, FaBed, FaChair,FaMapMarkerAlt, FaParking, FaShare,} from 'react-icons/fa';
+import Contact from '../components/Contact';
 
 // 7.54.55
 
