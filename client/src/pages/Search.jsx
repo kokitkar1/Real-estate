859fcea8ba13cacac6491424e3db3e5a5338415e
Sagar Kokitkar <101313853+kokitkar1@users.noreply.github.com>
2023-11-08 07:57:33 +0530

    Change in Search option

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,15 +1,77 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useNavigate} from 'react-router-dom'
 
 const Search = () => {
 
     const [sidebardata, setSidebardata] = useState({ searchTerm: '', type: 'all',parking: false, furnished: false, offer: false, sort: 'created_at', order: 'desc'})
+    const [loading, setLoding] = useState(false)
+    const [listings, setListings] = useState([])
+    const navigate = useNavigate()
 
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const searchTermFromUrl = urlParams.get('searchTerm');
+        const typeFromUrl = urlParams.get('type');
+        const parkingFromUrl = urlParams.get('parking');
+        const furnishedFromUrl = urlParams.get('furnished');
+        const offerFromUrl = urlParams.get('offer');
+        const sortTermFromUrl = urlParams.get('sort');
+        const orderFromUrl = urlParams.get('order');
+
+        if(searchTermFromUrl || typeFromUrl || parkingFromUrl || furnishedFromUrl || offerFromUrl || sortTermFromUrl || orderFromUrl){
+            setSidebardata({searchTerm : searchTermFromUrl || '',
+                            type: typeFromUrl || 'all',
+                            parking : parkingFromUrl === 'true' ? true : false,
+                            furnished: furnishedFromUrl === 'true' ? true : false,
+                            offer : offerFromUrl === 'true' ? true :false,
+                            sort: sortTermFromUrl || 'created_at',
+                            order: orderFromUrl || 'desc'})
+        }
+
+        const fetchListings = async () => {
+            setLoding(true);
+            const searchQuery = urlParams.toString();
+            const res = await fetch(`/api/listing/get?${searchQuery}`)
+            const data = res.json();
+            setListings(data);
+            setLoding(false)
+        }
+        fetchListings()
+    },[location.search])
 
     const handleChange = (e) => {
         if(e.target.id === 'all' || e.target.id === 'rent' || e.target.id === 'sale'){
             setSidebardata({...sidebardata, type: e.target.id})
         }
+
+        if(e.target.id === 'searchTerm'){
+            setSidebardata({...sidebardata, searchTerm : e.target.value})
+        }
+
+        if(e.target.id === 'parking' || e.target.id === 'furnished' || e.target.id === 'offer'){
+            setSidebardata({...sidebardata, [e.target.id] : e.target.checked || e.target.checked === 'true' ? true : false })
+        }
         
+        if(e.target.id === 'sort_order'){
+            const sort = e.target.value.split('_')[0] || 'created_at';
+            const order = e.target.value.split('_')[1] || 'desc';
+            setSidebardata({...sidebardata, sort, order})
+        }
+    }
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const urlParams = new URLSearchParams();
+        urlParams.set('searchTerm', sidebardata.searchTerm)
+        urlParams.set('type', sidebardata.type)
+        urlParams.set('parking', sidebardata.parking)
+        urlParams.set('furnished', sidebardata.furnished)
+        urlParams.set('offer', sidebardata.offer)
+        urlParams.set('sort', sidebardata.sort)
+        urlParams.set('order', sidebardata.order)
+        const searchQuery = urlParams.toString();
+        navigate(`/search?${searchQuery}`)
     }
 
 
@@ -17,7 +79,7 @@ const Search = () => {
     <div className=' flex flex-col md:flex-row'>
 
        <div className=" p-7 border-b-2 md:border-r-2 md:min-h-screen">
-            <form className=' flex flex-col gap-8'>
+            <form onSubmit={handleSubmit} className=' flex flex-col gap-8'>
                 <div className="flex items-center gap-2">
                     <label className=' whitespace-nowrap font-semibold'> Search Term:</label>
                     <input type="text" id="searchTerm" placeholder='Search...' className='border rounded-lg p-3 w-full' value={sidebardata.searchTerm} onChange={handleChange} />
@@ -67,7 +129,7 @@ const Search = () => {
        </div>
 
        <div className="">
-            <h1 className='w-screen border-b-2 text-3xl font-semibold p-3 text-slate-700 mt-5'>Listing Results: </h1>
+            <h1 className='border-b-2 text-3xl font-semibold p-3 text-slate-700 mt-5'>Listing Results: </h1>
        </div>
 
     </div>
