c898964e0d3c350e89a2c3180dcbfda6829b621d
Sagar Kokitkar <101313853+kokitkar1@users.noreply.github.com>
2023-11-11 07:07:51 +0530

    Add deploy prefix

--- api/index.js
+++ api/index.js
@@ -5,18 +5,27 @@ import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
+import path from 'path'
+
 
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL).then(() => {console.log("Connected to MongoDB");}).catch((err) => {console.log(err);});
 
+const __dirname = path.resolve()
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("/", (req, res) => res.send("Hello World!"));
+// app.get("/", (req, res) => res.send("Hello World!"));
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'client' , 'dist', 'index.html'))
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
